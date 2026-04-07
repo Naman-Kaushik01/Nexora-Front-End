8b86e6e3ca87ae3797a0710b38afe13da92576c6
Naman Kaushik <kaushiknaman68@gmail.com>
2026-04-08 00:05:18 +0530

    created profile page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Route path="/login" element ={<Login />} />
        <Route path="/home" element ={<Home/>} />
        <Route path="/" element ={<Home/>} />
+       <Route path="/profile" element ={<Profile/>} />
 
        
         <Route path="*" element ={<NotFound/>} />
